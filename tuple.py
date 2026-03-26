65b349828e0b94e575701da9013dbededbf212ee
laxmibhoomika12-max <laxmibhoomika12@gmail.com>
2026-03-26 12:56:37 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,18 @@
+mytuple=("prshant","ashish","rahul","sandip","komal","ankush","rajesh",23,3.25,"sandip")
+print(mytuple)
+print(type(mytuple))
+
+print(mytuple[0])
+
+mytuple[0]="sunil" #tuple is immutable we cannot change the value of tuple
+print(mytuple)
+
+     #differnce between list and tuple
+     #if we want to store data which is changeable we will use list data type 
+     #if we want to store data which is not changeable we will use tuple data type
+
+
+                                               
+
+
+
