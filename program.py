69061a178e77466803cff5f8b3f8ff724dd6e792
laxmibhoomika12-max <laxmibhoomika12@gmail.com>
2026-03-28 00:51:31 +0530

    Delete program.py

--- program.py
+++
@@ -1,6 +0,0 @@
-s ="Python is a high level programming language"
-print(s.lower()) #python is a high level programming language
-print(s.upper()) #PYTHON IS A HIGH LEVEL PROGRAMMING LANGUAGE
-print(s.swapcase()) #pYTHON IS A HIGH LEVEL PROGRAMMING LANGUAGE
-print(s.title()) #Python Is A High Level Programming Language
-print(s.capitalize()) #Python is a high level programming language
