5674c0cdee6c44db60667531dfe2271730c67e5f
laxmibhoomika12-max <laxmibhoomika12@gmail.com>
2026-03-28 00:50:22 +0530

    Delete temperature.py

--- temperature.py
+++
@@ -1,4 +0,0 @@
-#WAP to accept centigrade temperature and convert it into fahranite temerature
-c=float(input("Enter temperature in Centigrade:"))
-f=(c*9/5)+32
-print("Temperature in Fahranite= ",f)
